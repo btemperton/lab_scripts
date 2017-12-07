5dcedc29540c10e397bc97baae93a993763c6a29
bt273 <b.temperton@exeter.ac.uk>
2017-12-07 09:20:50 +0000

    added ability to create an index file mapping old to new

--- rename.fasta.py
+++ rename.fasta.py
@@ -2,21 +2,28 @@ import argparse
 import sys
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
+import pandas as pd
 
 def main():
 	parser = argparse.ArgumentParser()
 	
 	parser.add_argument('--infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 	parser.add_argument('--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+	parser.add_argument('--mapping_file', type=argparse.FileType('w'))
 	parser.add_argument('--prefix', type=str, required=True)
 	parser.add_argument('--no_desc', dest='no_desc', default=True, action='store_true')
 	parser.set_defaults(no_desc=False)
 	args = parser.parse_args()
 		
 	out_seqs = []
+	records = []
 	counter = 1
 	for record in SeqIO.parse(args.infile, 'fasta'):
-		record.id = "%s_%05d" % (args.prefix, counter)
+		new_id = "%s_%05d" % (args.prefix, counter)
+		if (args.mapping_file):
+			records.append((record.id, new_id))
+
+		record.id = new_id
 		if (args.no_desc):
 			record.description = ''
 		out_seqs.append(record)
@@ -26,6 +33,10 @@ def main():
 			out_seqs = []
 	
 	SeqIO.write(out_seqs, args.outfile, 'fasta')
+
+	if(args.mapping_file):
+		df = pd.DataFrame(records, columns=['old_id', 'new_id'])
+		df.to_csv(args.mapping_file, index=False)
 	
 if __name__ == '__main__':
     main()	
